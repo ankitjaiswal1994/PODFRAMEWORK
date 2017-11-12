45e114e8f21eb7463ceddeef8602dc7b3895f409
ankitjaiswal1994 <ankit.jaiswal@crownstack.com>
2017-11-12 17:47:14 +0530

    init

---
+++ PODFRAMEWORK/PODFRAMEWORK.h
@@ -0,0 +1,19 @@
+//
+//  PODFRAMEWORK.h
+//  PODFRAMEWORK
+//
+//  Created by Ankit Jaiswal on 12/11/17.
+//  Copyright © 2017 Ankit Jaiswal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PODFRAMEWORK.
+FOUNDATION_EXPORT double PODFRAMEWORKVersionNumber;
+
+//! Project version string for PODFRAMEWORK.
+FOUNDATION_EXPORT const unsigned char PODFRAMEWORKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PODFRAMEWORK/PublicHeader.h>
+
+
